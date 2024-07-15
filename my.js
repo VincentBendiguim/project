99f773657b0031206a408e91f62953ea0def6c23
bendiguim <bendiguimlevy@gmail.com>
2024-07-15 12:56:00 +0100

    first commit

---
+++ my.js
@@ -0,0 +1,13 @@
+// //Close the dropdown menu if the user click outside ofnit
+// window.onclick = function (event) {
+//     if (!event.target.matches('.usre-icon')) {
+//         var dropdown = document.getElementByIdName(dropdown)
+//         var i;
+//         for (i = 0; i < dropdown.lenght; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.style.display == "inline") {
+//                 openDropdown.style.display = "none";
+//             }
+//         }
+//     }
+// }
